ab2779d7c9163a03ecd0639a927c94dae0bcefd4
paul <paulentz@gmail.com>
2023-07-08 23:33:53 -0500

    did coding challenge week 2

---
+++ Coding_Challenge_Week_2/function FirstReverse.js
@@ -0,0 +1,13 @@
+function FirstReverse(str) {
+
+    arr = str.split('');
+    let revArr = arr.reverse();
+    str = revArr.join('');
+
+    return str;
+
+}
+
+console.log(FirstReverse('Paul'))
+
+//results url https://www.coderbyte.com/results/kjdkfjdakjfdjfdsakfjd:First%20Reverse:JavaScript
