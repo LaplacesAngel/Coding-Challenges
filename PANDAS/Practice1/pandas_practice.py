85270fb2637c707dd804b798c3939a04430e6280
paul <paulentz@gmail.com>
2023-11-15 23:20:26 -0600

    read in data

--- PANDAS/Practice1/pandas_practice.py
+++ PANDAS/Practice1/pandas_practice.py
@@ -8,3 +8,9 @@ def printTail(thing: pd.DataFrame) -> pd.DataFrame:
     print(f"the last ten rows are: \n", thing.tail(10))
 
 printTail(titanic)
+
+print(f"\n\n\n\n", titanic.dtypes, "\n\n\n\n", titanic.describe())
+
+titanic.to_excel("titanic.xlsx", sheet_name="passengers", index=False)
+
+printTail(titanic)
